83e541b360fcbd7daa6e04989b9c5a4b6fa4c56b
code-Miracle49 <chukwuemekamiracle12@gmail.com>
2023-01-14 01:56:36 +0100

    changes

---
+++ DemoApp/API.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DemoApp
+{
+    public class API
+    {
+
+        public List<string> demo_api()
+        {
+            string url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo";
+            var client = new RestClient(url);
+            var request = new RestRequest();
+            var content = client.ExecuteGet(request);
+            string jsonContent = content.Content!;
+            dynamic? result = JsonConvert.DeserializeObject<dynamic>(jsonContent);
+            var data = new List<string>();
+           foreach(var obj in result["Global Quote"])
+            {
+                data.Add(obj.Name + ": " + obj.Value);
+            }
+            return data;
+        }
+    }
+}
---
+++ DemoApp/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.LocalNotification;
+
+namespace DemoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+        bool timervalue;
+        void Button1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            GetData();
+            timervalue = true;
+            Device.StartTimer(TimeSpan.FromMinutes(5), () =>
+                {
+                    // called every 5 minutes
+                    GetData();
+                    SendNotification();
+                    return timervalue; // return true of false. true to repeat count, false to stop counting;
+                });
+        }
+
+        void Button2_Clicked(System.Object sender, System.EventArgs e)
+        {
+            timervalue = false; //Stop Timer
+        }
+
+        void GetData()
+        {
+            var data = new List<string>();
+            API api = new API();
+            data = api.demo_api();
+            string label = string.Empty;
+            string crlf = "\r\n";
+            foreach (var item in data)
+            {
+                label += item + crlf;
+            }
+            API_Data.Text = label;
+        }
+        void SendNotification()
+        {
+            var notification = new NotificationRequest
+            {
+                BadgeNumber = 1,
+                Description = "Demo App Data Updated",
+                Title = "Notification",
+                ReturningData = "DummyData",
+                NotificationId = 1234,
+            };
+            LocalNotificationCenter.Current.Show(notification);
+        }
+    }
+}
